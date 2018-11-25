ed4759a6cead8394674b7789548f869b06eded71
omryko <omryyy@gmail.com>
2018-11-26 01:56:54 +0200

    fix email sent bug

--- app/models/user.js
+++ app/models/user.js
@@ -57,7 +57,7 @@ var userSchema = new mongoose.Schema({
 userSchema.methods.sendMail = function (user) {
   var to = user.email,
     subject = 'Welcome to PROGO',
-    message = 'To activate your email click <a href="' + process.env.URL + '/v/' + user._id + '/' + user.token + '">here</a>'; //activation link
+    message = 'To activate your email click <a href="' + process.env.URL + "/v/" + user._id + '/' + user.token + '">here</a>'; //activation link
   mail(to, subject, message);
 }
 //get rendom image
